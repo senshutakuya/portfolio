38b066a7ad844bd2abb318d35a65e583d36e871c
senshutakuya <83773245+senshutakuya@users.noreply.github.com>
2023-06-14 02:42:51 +0900

    Delete WELP-new_welp directory

--- WELP-new_welp/WELP/config/login_required.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/init.php');
-require_once($root . 'functions/user.php');
-
-if (empty($_SESSION['user_id']) or getUserFromId($_SESSION['user_id']) == null){
-    header('Location: /login');
-    exit;
-}
