73e8e98fc9f817e7dfac15c45c5a4c9940a62c7e
senshutakuya <83773245+senshutakuya@users.noreply.github.com>
2023-06-14 02:46:42 +0900

    Create config.php

---
+++ WELP-new_welp/WELP/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'TITLE' => 'WELP',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'welp',
+    'DB_USER' => 'welp_admin',
+    'DB_PASSWORD' => 'welp_admin',
+    'DEBUG_MODE' => true,
+    'MAINTENANCE_MODE' => false
+];
